b4a9b670f18aa26d8bf3a830f5923caa072ee1b5
Mohamed Alaya <alayamohamed@outlook.com>
2022-03-01 00:40:26 +0000

    Login page and update theme signup form

--- src/components/Signupform.vue
+++ src/components/Signupform.vue
@@ -1,73 +1,24 @@
 <template>
-  <form>
-    <label for="chk" aria-hidden="true"> Sign up </label>
-    <br />
-
-    <label> Email </label>
-    <input
-      type="email"
-      required
-      v-model="email"
-      placeholder="Type your email .. "
-    />
-    <label> Password </label>
-    <input type="password" required v-model="password" />
-
-    <label> Role : </label>
-    <select v-model="role">
-      <options value="WAHA"> Work from Home </options>
-      <options value="WFO"> Work from Office </options>
-    </select>
-
-    <div class="terms">
-      <input type="checkbox" required v-model="terms" />
-      <label> Accpet terms and conditions </label>
-    </div>
-
-    <div>
-      <input type="checkbox" required value="HR" v-model="departements" />
-      <label> Human Resources </label>
-      <input type="checkbox" required value="Acc&F " v-model="departements" />
-      <label> Accounting and finance </label>
-      <input type="checkbox" required value="R&D" v-model="departements" />
-      <label> R&D departement </label>
-      <input type="checkbox" required value="M&S" v-model="departements" />
-      <label> Marketing & Sales </label>
-      <input
-        type="checkbox"
-        required
-        value="Customer srv"
-        v-model="departements"
-      />
-      <label> Customer service </label>
-    </div>
-
-    <button>Sign up</button>
-  </form>
-
-  <div class="login">
-    <form>
-      <label for="chk" aria-hidden="true">Login</label>
-      <br />
-
-      <label> Email </label>
-      <input
-        type="email"
-        required
-        v-model="email"
-        placeholder="Type your email .. "
-      />
-      <label> Password </label>
-      <input type="password" required v-model="password" />
-      <button>Login</button>
-    </form>
+  <div class="background">
+    <div class="shape"></div>
+    <div class="shape"></div>
   </div>
+  <form>
+    <h3>Subscribe Here</h3>
+    <label for="Name">Name</label>
+    <input type="text" placeholder="Name" id="name" />
+    <label for="LastName">Name</label>
+    <input type="text" placeholder="LastName" id="lastname" />
+    <label for="username">Username</label>
+    <input type="text" placeholder="Email or Phone" id="username" />
 
-  <p>Email : {{ email }}</p>
-  <p>password : {{ password }}</p>
-  <p>Role : {{ role }}</p>
-  <p>terms accpected : {{ terms }}</p>
-  <p>Department : {{ department }}</p>
+    <label for="password">Password</label>
+    <input type="password" placeholder="Password" id="password" />
+    <label for="password">Confirm Password</label>
+    <input type="password" placeholder="Confirm Password" id="password" />
+
+    <button>Create Account</button>
+  </form>
 </template>
 
 <script>
@@ -84,93 +35,129 @@ export default {
 };
 </script>
 
-<style>
-form {
-  max-width: 420px;
-  margin: 30px auto;
-  background-color: #ffa07a;
-  text-align: left;
-  padding: 40px;
-  border-radius: 10px;
+<style media="screen">
+      *,
+*:before,
+*:after{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
 }
-Label {
-  color: #af4a68;
-  display: inline-block;
-  margin: 25px 0 15px;
-  font-size: 0.6em;
-  text-transform: uppercase;
-  letter-spacing: 1px;
-  font-weight: bold;
-  cursor: pointer;
-  transition: 0.5s ease-in-out;
+body{
+    background-color: #080710;
 }
-input,
-select {
-  display: block;
-  padding: 10px 6px;
-  width: 100%;
-  box-sizing: border-box;
-  border: none;
-  border-bottom: 1px solid #ddd;
-  color: #555;
+.background{
+    width: 430px;
+    height: 520px;
+    position: absolute;
+    transform: translate(-50%,-50%);
+    left: 50%;
+    top: 50%;
 }
-input[type="checkbox"] {
-  display: inline-block;
-  width: 16px;
-  margin: 0 10px 0 0;
-  position: relative;
-  top: 2px;
+.background .shape{
+    height: 200px;
+    width: 200px;
+    position: absolute;
+    border-radius: 50%;
 }
-
-#chk {
-  display: none;
+.shape:first-child{
+    background: linear-gradient(
+        #1845ad,
+        #23a2f6
+    );
+    left: -80px;
+    top: -80px;
 }
-.signup {
-  position: relative;
-  width: 100%;
-  height: 100%;
+.shape:last-child{
+    background: linear-gradient(
+        to right,
+        #ff512f,
+        #f09819
+    );
+    right: -70px;
+    bottom: -270px;
 }
-
-button {
-  width: 60%;
-  height: 40px;
-  margin: 10px auto;
-  justify-content: center;
-  display: block;
-  color: #fff;
-  background: #573b8a;
-  font-size: 1em;
-  font-weight: bold;
-  margin-top: 20px;
-  outline: none;
-  border: none;
-  border-radius: 5px;
-  transition: 0.2s ease-in;
-  cursor: pointer;
+form{
+    height: 700px;
+    width: 400px;
+    background-color: rgba(255,255,255,0.13);
+    position: absolute;
+    transform: translate(-50%,-50%);
+    top: 60%;
+    left: 50%;
+    border-radius: 10px;
+    backdrop-filter: blur(10px);
+    border: 2px solid rgba(255,255,255,0.1);
+    box-shadow: 0 0 40px rgba(8,7,16,0.6);
+    padding: 50px 35px;
 }
-button:hover {
-  background: #6d44b8;
+form *{
+    font-family: 'Poppins',sans-serif;
+    color: #ffffff;
+    letter-spacing: 0.5px;
+    outline: none;
+    border: none;
 }
-
-.login {
-  height: 460px;
-  background: #eee;
-  border-radius: 60% / 10%;
-  transform: translateY(-180px);
-  transition: 0.8s ease-in-out;
-}
-.login label {
-  color: #573b8a;
-  transform: scale(0.6);
+form h3{
+    font-size: 32px;
+    font-weight: 500;
+    line-height: 42px;
+    text-align: center;
 }
 
-#chk:checked ~ .login {
-  transform: translateY(-500px);
+label{
+    display: block;
+    margin-top: 30px;
+    font-size: 16px;
+    font-weight: 500;
 }
-#chk:checked ~ .login label {
-  transform: scale(1);
+input{
+    display: block;
+    height: 50px;
+    width: 100%;
+    background-color: rgba(255,255,255,0.07);
+    border-radius: 3px;
+    padding: 0 10px;
+    margin-top: 8px;
+    font-size: 14px;
+    font-weight: 300;
 }
-#chk:checked ~ .signup label {
-  transform: scale(0.6);
+::placeholder{
+    color: #e5e5e5;
 }
-</style>
+button{
+    margin-top: 50px;
+    width: 100%;
+    background-color: #ffffff;
+    color: #080710;
+    padding: 15px 0;
+    font-size: 18px;
+    font-weight: 600;
+    border-radius: 5px;
+    cursor: pointer;
+}
+.social{
+  margin-top: 30px;
+  display: flex;
+}
+.social div{
+  background: red;
+  width: 150px;
+  border-radius: 3px;
+  padding: 5px 10px 10px 5px;
+  background-color: rgba(255,255,255,0.27);
+  color: #eaf0fb;
+  text-align: center;
+}
+.social div:hover{
+  background-color: rgba(255,255,255,0.47);
+}
+.social .fb{
+  margin-left: 25px;
+}
+.social i{
+  margin-right: 4px;
+}
+
+    </style>
+
